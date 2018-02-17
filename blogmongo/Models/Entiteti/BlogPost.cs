bb068e529dcdbfadcbffb7c5ea926f4cc0391e1b
SAVO-PC\Savo <savo.vujacic@diyomisoft.com>
2018-02-17 19:03:08 +0100

    new

---
+++ blogmongo/Models/Entiteti/BlogPost.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace NBPBlogoviTest.Models.Entiteti
+{
+    public class BlogPost
+    {
+        public ObjectId Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ObjectId Author { get; set; }  // ili MongoDBRef
+        public List<string> Tags { get; set; }
+        public int Likes { get; set; } 
+        public List<Comment> Comments { get; set; }
+
+    }
+}
+
+/*
+ *  {
+   _id: POST_ID
+   title: TITLE_OF_POST, 
+   description: POST_DESCRIPTION,
+   by: POST_BY,
+   url: URL_OF_POST,
+   tags: [TAG1, TAG2, TAG3],
+   likes: TOTAL_LIKES, 
+   comments: [	
+      {
+         user:'COMMENT_BY',
+         message: TEXT,
+         dateCreated: DATE_TIME,
+         like: LIKES 
+      },
+      {
+         user:'COMMENT_BY',
+         message: TEXT,
+         dateCreated: DATE_TIME,
+         like: LIKES
+      }
+   ]
+}
+ */
---
+++ blogmongo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(blogmongo.Startup))]
+namespace blogmongo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
